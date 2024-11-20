34af58d665a62e2aabe4907f60642de87708ad50
Kamar <kamardoblibennani@gmail.com>
2024-11-20 12:49:21 +0100

    project setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,11 +14,12 @@ import { getDatabase, provideDatabase } from '@angular/fire/database';
   imports: [
     BrowserModule,
     AppRoutingModule,
+  ],
+  providers: [
     provideFirebaseApp(() => initializeApp({"projectId":"smartbox-a4210","appId":"1:492144900610:web:6244166539a47dc77dc51b","databaseURL":"https://smartbox-a4210-default-rtdb.europe-west1.firebasedatabase.app","storageBucket":"smartbox-a4210.firebasestorage.app","apiKey":"AIzaSyDVTbol-gCz6oQP1TkD8jo3k8M-v3NDBNA","authDomain":"smartbox-a4210.firebaseapp.com","messagingSenderId":"492144900610","measurementId":"G-9W08B7JYQL"})),
     provideAuth(() => getAuth()),
     provideDatabase(() => getDatabase())
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
